840ec57bd2756a375d16e7f10d75e3e8b09fd378
alumno <alumno>
2019-02-11 20:38:07 -0300

    Introduccion IF (quinto ejercicio)

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,23 @@
 function mostrar()
 {
 //tomo la edad  
+var laEdad;
+
+laEdad = edad.value;
+laEdad= parseInt(laEdad);
+
+if (! (laEdad >12 && laEdad <18))
+{
+	alert ("Usted No es un adolecente");
+}
+
+//Ultizando ||(No)
+
+if (laEdad <13 || laEdad >17)
+
+{
+	alert ("Usted No es un adolecente");
+}
 
 
 }//FIN DE LA FUNCIÓN
