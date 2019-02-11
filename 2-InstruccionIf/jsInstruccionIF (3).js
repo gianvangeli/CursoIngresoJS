c1f669f5716220d8e212efbbd89188d353cfd370
alumno <alumno>
2019-02-11 20:05:32 -0300

    Introduccion IF (tercer ejercicio)

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -2,5 +2,20 @@ function mostrar()
 {
 //tomo la edad  
 
+var laEdad;
+
+laEdad = edad.value;
+laEdad= parseInt(laEdad);
+
+if (laEdad >17)
+
+{
+alert ("Usted es mayor de edad");
+}
+else
+{
+	alert ("Usted es menor de edad");
+}
+
 
 }//FIN DE LA FUNCIÓN
