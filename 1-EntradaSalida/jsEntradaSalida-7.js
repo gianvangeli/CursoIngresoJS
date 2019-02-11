d9f7e505118b6a16a42be7d256d01d06afbaec79
alumno <alumno>
2019-02-11 19:12:06 -0300

    ejercicio 7 terminado

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -3,22 +3,75 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
-{	
-	
+{
+var primerNumero;
+var segundoNumero;
+var suma;
+
+primernumero=numeroUno.value;
+segundonumero=numeroDos.value;
+
+primernumero=parseInt(primernumero);
+segundonumero=parseInt(segundonumero);
+
+suma = primernumero + segundonumero;
+
+alert ("La suma es" +suma);
 }
 
 function restar()
 {
+	var primernumero;
+	var segundonumero;
+	var resta;
+   
+primernumero=numeroUno.value;
+segundonumero=numeroDos.value;
+
+primernumero=parseInt(primernumero);
+segundonumero=parseInt(segundonumero);
+
+resta = primernumero - segundonumero;
+
+alert ("La resta es" +resta);
 	
 }
 
 function multiplicar()
 { 
+
+var primernumero;
+var segundonumero;
+var multiplicacion;
+
+primernumero=numeroUno.value;
+segundonumero=numeroDos.value;
+
+primernumero=parseInt(primernumero);
+segundonumero=parseInt(segundonumero);
+
+multiplicacion= primernumero * segundonumero;
+
+alert ("La multiplicacion es" +multiplicacion);
+
 	
 }
 
 function dividir()
 {
+	var primernumero;
+	var segundonumero;
+	var divicion;
+
+	primernumero=numeroUno.value;
+	segundonumero=numeroDos.value;
+
+	primernumero=parseInt(primernumero);
+	segundonumero=parseInt(segundonumero);
+
+	divicion= primernumero / segundonumero;
+
+	alert ("La divicion es" + divicion);
 	
 }
 
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,17 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+var Sueldo;
+var Resultado;
+
+Sueldo=sueldo.value;
+Resultado=resultado.value;
+
+Sueldo=parseInt(Sueldo)-1,1;
+
+Resultado=Sueldo-
+
+
 
 	
 }
