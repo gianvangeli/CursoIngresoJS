0514c45775c6d3ae9e68f6295aeb173efb80a20a
gianvangeli <gian.vangeli13@gmail.com>
2019-02-13 14:59:53 -0300

    Introduccion IF (septimo ejercicio)

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -2,6 +2,29 @@ function mostrar()
 {
 //tomo la edad  
 
+    var laEdad;
+	var estadocivil;
+
+	laEdad = edad.value;
+	laEdad = parseInt(laEdad);
+	estadocivil = estadoCivil.value;
+
+	if(laEdad < 18)
+	{
+		if(estadocivil != "Soltero")
+		{
+			alert("Es muy pequeño para NO ser soltero");
+        }
+    }
+
+
+
+
+   
+    
+
+
+
 	
 
 
