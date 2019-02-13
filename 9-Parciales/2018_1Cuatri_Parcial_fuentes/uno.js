8fe8cb15804e3e9e5abb22c7f2c1c2c30e120134
gianvangeli <gian.vangeli13@gmail.com>
2019-02-13 14:04:14 -0300

    Parcial primer ejercicio

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,19 @@
 
 function mostrar()
 {
+    var largo;
+    var ancho;
+    var perimetro;
+
+    largo = prompt ("Ingrese el largo");
+    
+    ancho = prompt ("Ingrese el ancho");
+
+    largo = parseInt(largo);
+    ancho = parseInt(ancho);
+
+    perimetro = largo + ancho ;
+
+    alert("El perimetro es; " +perimetro);
 
 }
