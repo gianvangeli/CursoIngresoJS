55c0f4af0dd824b63ed1eb0dc852a5bc04477f21
gianvangeli <gian.vangeli13@gmail.com>
2019-02-19 23:50:11 -0300

    Ejercicio 4 parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,46 @@
 function mostrar()
 {
+    var primerNumero;
+    var segundoNumero;
+    var suma;
+
+    primerNumero = prompt ("Ingrese el primer numero");
+    segundoNumero = prompt ("Ingrese el segundo numero");
+
+    if (primerNumero == segundoNumero)
+    {
+        alert (primerNumero+segundoNumero);
+    }
+    primerNumero =parseInt (primerNumero);
+    segundoNumero = parseInt (segundoNumero);
+
+    if (primerNumero > segundoNumero)
+    {
+        suma = primerNumero - segundoNumero;
+        alert ("La resta es" + suma);
+    }
+    if (primerNumero < segundoNumero)
+    {
+        suma = primerNumero + segundoNumero;
+
+        if (suma >10)
+        {
+            alert ("la suma es" +suma+"y supero el 10");
+        }
+        else 
+        {
+            alert ("La suma es:" +suma);
+        }
+    }
+
+
+
+
+    
+    
+
+
+
+
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,49 @@
 function mostrar()
 {
+    var primerNumero;
+    var segundoNumero;
+    var suma;
+
+    primerNumero = prompt ("Ingrese el primer numero");
+    segundoNumero = prompt ("Ingrese el segundo numero");
+
+    if (primerNumero == segundoNumero)
+    {
+        alert (primerNumero + segundoNumero);
+    }
+
+    primerNumero = parseInt (primerNumero);
+    segundoNumero = parseInt (segundoNumero);
+
+    if (primerNumero > segundoNumero)
+    {
+        suma = primerNumero / segundoNumero;
+        alert ("La division es"+suma);
+    }
+    if (primerNumero < segundoNumero)
+    {
+        suma = primerNumero+segundoNumero;
+
+        if (suma <50)
+        {
+            alert ("la suma es" +suma+"y es menor a 50");
+
+        }
+        else
+        {
+            alert ("La suma es:"+suma);
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
 
 }
